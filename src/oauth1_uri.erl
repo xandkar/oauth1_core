74bf7155bf75775656c40d13e56752c1d28118d6
Siraaj Khandkar <siraaj@khandkar.net>
2014-07-09 23:41:05 -0400

    Fix incorrect typing of oauth1_uri:of_bin/1

--- src/oauth1_uri.erl
+++ src/oauth1_uri.erl
@@ -107,26 +107,31 @@ to_bin(#t
     >>.
 
 -spec of_bin(binary()) ->
-    t().
+    hope_result:t(t(), no_scheme).
 of_bin(<<URIString/binary>>) ->
     case http_uri:parse(binary_to_list(URIString))
     of  {error, _}=Error ->
             Error
     ;   {ok, {Scheme, UserInfo, Host, Port, Path, Query}}
         when Scheme =:= http orelse Scheme =:= https ->
-            UserInfoBin  = list_to_binary(UserInfo),
+            UserInfoOpt =
+                case UserInfo
+                of  [] -> none
+                ;   [_|_] -> {some, list_to_binary(UserInfo)}
+                end,
             HostBin      = list_to_binary(Host),
             PathBin      = list_to_binary(Path),
             QueryBin     = list_to_binary(Query),
-            #t
-            { scheme         = Scheme
-            , user           = UserInfoBin
-            , host           = HostBin
-            , port           = Port
-            , path_and_query = <<PathBin/binary, QueryBin/binary>>
-            , path           = PathBin
-            , query          = cow_qs:parse_qs(QueryBin)
-            }
+            T = #t
+                { scheme         = Scheme
+                , user           = UserInfoOpt
+                , host           = HostBin
+                , port           = Port
+                , path_and_query = <<PathBin/binary, QueryBin/binary>>
+                , path           = PathBin
+                , query          = cow_qs:parse_qs(QueryBin)
+                },
+            {ok, T}
 
     end.
 
