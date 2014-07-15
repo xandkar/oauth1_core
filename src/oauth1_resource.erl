a802a1a39706318ca74c4ede6f7e488130d0f3b8
Siraaj Khandkar <siraaj@khandkar.net>
2014-07-14 22:48:49 -0400

    Remove obsolete serialization helpers.

--- src/oauth1_resource.erl
+++ src/oauth1_resource.erl
@@ -13,10 +13,6 @@
     % Access
     , get_realm/1
     , get_uri/1
-
-    % Serialize (for internal storage purposes only!)
-    , to_props/1
-    , of_props/1
     ]).
 
 
@@ -33,10 +29,6 @@
 
 -opaque t() ::
     #t{}.
-
-
--define(SERIALIZATION_KEY_REALM, <<"realm">>).
--define(SERIALIZATION_KEY_URI  , <<"uri">>).
 
 
 -spec cons(realm(), uri()) ->
@@ -56,23 +48,3 @@ get_realm(#t{realm=Realm}) ->
     uri().
 get_uri(#t{uri=URI}) ->
     URI.
-
--spec to_props(t()) ->
-    [{binary(), binary()}].
-to_props(#t{realm = <<Realm/binary>>, uri=URI}) ->
-    [ {?SERIALIZATION_KEY_REALM , Realm}
-    , {?SERIALIZATION_KEY_URI   , oauth1_uri:to_bin(URI)}
-    ].
-
--spec of_props([{binary(), binary()}]) ->
-    t().
-of_props(Props1) ->
-    KeyRealm = ?SERIALIZATION_KEY_REALM,
-    KeyURI   = ?SERIALIZATION_KEY_URI,
-    {value, {KeyRealm, Realm },  Props2} = lists:keytake(KeyRealm, 1, Props1),
-    {value, {KeyURI  , URIBin}, _Props3} = lists:keytake(KeyURI  , 1, Props2),
-    {ok, URI} = oauth1_uri:of_bin(URIBin),
-    #t
-    { realm = Realm
-    , uri   = URI
-    }.
